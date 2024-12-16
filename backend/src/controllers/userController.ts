1d1763376781aad5401864491f8410d1eeba9ca8
Haruto Tanaka <hamuchan214@gmail.com>
2024-12-16 14:08:36 +0900

    add: userControl func

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -10,4 +10,35 @@ router.get("/", async(req: Request, res: Response) =>{
     res.json({ users });
 });
 
+router.get("/:id", async(req: Request, res: Response) => {
+    const user_id = await prisma.user.findUnique({
+        where: {id: parseInt(req.params?.id)},
+    });
+    res.json({ user_id });
+});
+
+router.post("/", async(req: Request, res: Response) => {
+    const { name, email} = req.body;
+    const user = await prisma.user.create({
+        data: { name, email},
+    });
+    res.json({ user });
+});
+
+router.put("/:id", async(req: Request, res: Response) => {
+    const { name, email } = req.body;
+    const user = await prisma.user.update({
+        where: {id: parseInt(req.params?.id)},
+        data: { name, email},
+    });
+    res.json({ user });
+})
+
+router.delete("/:id", async(req: Request, res: Response) => {
+    const user = await prisma.user.delete({
+        where: { id: parseInt(req.params?.id) },
+    });
+    res.json({ user });
+});
+
 export default router
